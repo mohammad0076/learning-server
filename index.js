79aa15a971aa3da2d1fe204947dae1105b723879
mohammad0076 <mohammadullahpatwary.com@gmail.com>
2022-10-27 06:04:09 +0600

    r

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5000;
+const data = require('./data/data.json')
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log('Learning server', port)
+})
+app.get('/courses', (req, res) => {
+    res.send(data)
+})
